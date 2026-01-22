69f235c5a53cdbef186351b94bd968293679166c
Alvaro Ortega <alvaro.ortega@et.bo>
2026-01-22 11:30:19 -0400

    changed URL

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -1,6 +1,10 @@
 export const environment = {
    production: true,
    image_logo: 'logo_comcar.png',
-   server_url: 'https://emcar-mineral-backtend.onrender.com/api/v1',
-   server_security_url: 'https://emcar-mineral-backtend.onrender.com/api/v1',
+   server_url: 'https://emcar-mineral-backtend-ecav.onrender.com/api/v1',
+   server_security_url: 'https://emcar-mineral-backtend-ecav.onrender.com/api/v1',
+
+   // URL 
+   // server_url: 'https://emcar-mineral-backtend.onrender.com/api/v1',
+   // server_security_url: 'https://emcar-mineral-backtend.onrender.com/api/v1',
 };
